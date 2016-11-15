b365fc6a95486f8c7710152b8a714ff20e0e3514
zhongxiang <zhongxiang@pku.edu.cn>
2016-11-15 10:56:10 +0800

    merge

---
+++ test1.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import os
+import matplotlib.pyplot as plt
+
+class man():
+    @staticmethod
+    def mans():
+        print 'i am a man!'
+
+    def setsize(self,size):
+        self.size = size
+
+    def getsize(self):
+        return self.size
+
+    Size = property(getsize, setsize)
+
+myman = man()
+myman.size = 10
+print myman.size
+man().mans()
+
+os.chdir('C:\Users\dell\Desktop')
+print os.getcwd()
+G = nx.random_graphs.barabasi_albert_graph(30,1)
+nx.draw(G)
+plt.savefig("ba.png")
+plt.show()    
